bea859b5744e61bbd561c2bb8b2f13d737723198
Orpine <Orpine@msn.com>
2014-05-02 22:23:37 +0800

    Add ex7

--- Machine Learning/mlclass-ex6-005/mlclass-ex6/dataset3Params.m
+++ Machine Learning/mlclass-ex6-005/mlclass-ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+Cv = [0.01 0.03 0.1 0.3 1 3 10 30];
+sigmav = [0.01 0.03 0.1 0.3 1 3 10 30];
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,6 +23,19 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+merror = inf;
+for i = 1: length(Cv)
+    for j = 1: length(sigmav)
+        model= svmTrain(X, y, Cv(i), @(x1, x2) gaussianKernel(x1, x2, sigmav(j))); 
+        predictions = svmPredict(model, Xval);
+        error = mean(double(predictions ~= yval));
+        if (error < merror)
+            merror = error;
+            C = Cv(i);
+            sigma = sigmav(j);
+        end
+    end
+end            
 
 
 
--- Machine Learning/mlclass-ex6-005/mlclass-ex6/processEmail.m
+++ Machine Learning/mlclass-ex6-005/mlclass-ex6/processEmail.m
@@ -96,8 +96,7 @@ while ~isempty(email_contents)
     % Note: You can use strcmp(str1, str2) to compare two strings (str1 and
     %       str2). It will return 1 only if the two strings are equivalent.
     %
-
-
+    word_indices = [word_indices; find(strcmp(vocabList, str))];
 
 
 
